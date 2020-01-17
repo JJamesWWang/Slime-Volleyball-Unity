eba76698f438f3f0ed7bd0fa03592f8e65d505ec
James S. Wang <JJamesWWang@gmail.com>
2020-01-17 04:33:40 -0500

    Remodel system using game events

--- Assets/Scripts/Gameplay/GameEvent.cs
+++ Assets/Scripts/Gameplay/GameEvent.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class GameEvent : MonoBehaviour
 {
     // Game state related
-    public const string GAME_PAUSED = "GAME_PAUSED";
-    public const string GAME_UNPAUSED = "GAME_UNPAUSED";
-    public const string POINT_STARTED = "POINT_STARTED";
-    public const string POINT_ENDED = "POINT_ENDED";
-    public const string LEFT_TEAM_SCORED = "LEFT_TEAM_SCORED";
-    public const string RIGHT_TEAM_SCORED = "RIGHT_TEAM_SCORED";
-    public const string GAME_ENDED = "GAME_ENDED";
-
+    public const string GAME_STARTED = "GAME_STARTED";      //<>
+    public const string POINT_STARTED = "POINT_STARTED";    //<Side>
+    public const string POINT_ENDED = "POINT_ENDED";        //<Side>
+    public const string GAME_ENDED = "GAME_ENDED";          //<Side>
+    public const string GAME_PAUSED = "GAME_PAUSED";        //<>
+    public const string GAME_UNPAUSED = "GAME_UNPAUSED";    //<>
+    public const string GAME_RESET = "GAME_RESET";          //<>
 
     // Volleyball related
-    public const string PLAYER_CONTACT = "PLAYER_CONTACT";
-    public const string WALL_CONTACT = "WALL_CONTACT";
-    public const string SCORE_AREA_CONTACT = "SCORE_AREA_CONTACT";
-    public const string SPIKE_HIT = "SPIKE_HIT";
+    public const string PLAYER_CONTACT = "PLAYER_CONTACT";  //<Volleyball, PlayerController>
+    public const string WALL_CONTACT = "WALL_CONTACT";      //<Volleyball, Tilemap>
+    public const string SCORE_AREA_CONTACT = "SCORE_AREA_CONTACT";      //<Volleyball, ScoreArea>
+    public const string SPIKE_HIT = "SPIKE_HIT";            //<Volleyball, PlayerController>
+
+    // UI related
+
+    public const string SCORE_UPDATED = "SCORE_UPDATED";    //<int, int>
 
 }
